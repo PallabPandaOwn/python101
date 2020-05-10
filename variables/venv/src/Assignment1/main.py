1351edfcd9991ae9e52fa6f5ab971aa08a886535
Pallab Panda <Pallab.Panda@gmail.com>
2020-05-10 22:53:37 +0530

    global vs local variables
    Signed-off-by: Pallab Panda <Pallab.Panda@gmail.com>

---
+++ variables/venv/src/Assignment1/main.py
@@ -0,0 +1,13 @@
+# Listing out different characteristics  of a song
+
+Artist ="Shyam kumar" # Artist of song
+Genre = "Jazz" # gener of song
+YearReleased = 2020 # year of realesed
+Duration = 120.5 # duration of song
+Music = "ABC Company" # music company
+
+print(Artist)
+print(Genre)
+print(YearReleased)
+print(Duration)
+print(Music)
--- variables/venv/src/variables/variables.py
+++ variables/venv/src/variables/variables.py
@@ -1,5 +1,19 @@
 
-
+#Global fucntions
 one = 1
-one = 2
+One = 2
 print(one,One)
+
+stringvar = 'hello world'
+print(stringvar)
+
+def function1():
+    var1 = 1
+
+    return
+
+# mouse + cat + dog = "small"+"medium"+"large"
+# print("cat)
+
+function1()
+print(var1)#error because of out of scope
